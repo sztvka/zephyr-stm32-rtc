1b582a8fcf25129660de3ebfb659cc583b2eb055
sztvka <sztukaa@protonmail.com>
2023-03-16 17:40:33 +0100

    rtc timer reset persistence to be tested with battery

--- src/main.c
+++ src/main.c
@@ -10,16 +10,20 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/counter.h>
 #include <string.h>
+#include <zephyr/posix/time.h>
 #define TIMER DT_INST(0, st_stm32_rtc)
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   3000
+#define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 const struct device *const counter_dev = DEVICE_DT_GET(TIMER);
+time_t compile_timestamp = 1678984720;
+struct timespec time_now_ts;
+
 void main(void)
 {
 	int ret;
@@ -41,10 +45,10 @@ void main(void)
     };
 
 
-    if(counter_start(counter_dev)!=0) {
-        printk("counter start failed.\n");
-        return;
-    };
+    //if(counter_start(counter_dev)!=0) {
+   //     printk("counter start failed.\n");
+   //     return;
+  //  };
 
     uint32_t *ticks = k_malloc(sizeof(uint32_t));
     memset(ticks, 0, sizeof(uint32_t));
@@ -62,7 +66,13 @@ void main(void)
             return;
         }
         else{
+            struct tm   ttm;
+
+            time_t time_now = compile_timestamp + *ticks + 3600;
+            ttm = * gmtime(&time_now);
+
             printk("counter value: %d\n\r", *ticks);
+            printk("Current time: %02d:%02d:%02d\n\r", ttm.tm_hour, ttm.tm_min, ttm.tm_sec);
         }
 		k_msleep(SLEEP_TIME_MS);
 	}
